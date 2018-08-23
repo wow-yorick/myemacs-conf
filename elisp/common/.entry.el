a9390f8503c7f4aeade96ba46ba3e96b2660f4d7
Yorick.yuan <wowyorick@126.com>
2018-08-23 15:54:19 +0800

    emacs config

--- elisp/common/.entry.el
+++ elisp/common/.entry.el
@@ -10,7 +10,7 @@
 (add-to-list 'load-path (concat common-config-dir "/color-theme"))
 (require 'color-theme)
 (color-theme-initialize)
-(color-theme-arjen)
+;;(color-theme-arjen)
 
 ;;;;;;;;;;;;;;;;;;;;;;;
 ;; custom set variable
@@ -57,3 +57,54 @@
 ;;(setq default-input-method "chinese-pyim")
 ;;(global-set-key (kbd "C-\\") 'toggle-input-method)
 
+;;; auto-save
+(require 'auto-save)
+
+;;; Code:
+
+;; (auto-save-enable)
+;; (setq auto-save-silent t)
+;; (setq auto-save-delete-trailing-whitespace t)
+
+;; (provide 'init-auto-save)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blick-cursor-mode nil)
+ '(column-number-mode t)
+ '(custom-safe-themes
+   (quote
+    ("11e57648ab04915568e558b77541d0e94e69d09c9c54c06075938b6abc0189d8" default)))
+ '(delete-key-deletes-forward t)
+ '(global-linum-mode t)
+ '(gud-gdb-command-name "gdb --annotate=1")
+ '(large-file-warning-threshold nil)
+ '(line-number-mode f)
+ '(make-backup-files nil)
+ '(menu-bar-mode 0)
+ '(mouse-yank-at-point t)
+ '(package-selected-packages
+   (quote
+    (color-theme s neotree highlight-parentheses flycheck evil elisp-format)))
+ '(tool-bar-mode 0)
+ '(visible-bell t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(color-theme-aalto-light)
+(display-time-mode 1) ;; 常显 
+(setq display-time-24hr-format t) ;;格式 
+(setq display-time-day-and-date t) ;;显示时间、星期、日期
+
+(tool-bar-mode 0) 
+(menu-bar-mode 0) 
+(scroll-bar-mode 0)
+
+(global-hl-line-mode 1)
+;;; .emacs ends here
--- elisp/golang/.entry.el
+++ elisp/golang/.entry.el
@@ -104,3 +104,5 @@
 ;; (setq flycheck-gometalinter-enable-linters '("golint"))
 ;; Set different deadline (default: 5s)
 (setq flycheck-gometalinter-deadline "10s")
+
+(require 'go-guru)
