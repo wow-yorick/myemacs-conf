484dcf7f46bf07d98b29db5b7d6da52e7c529b66
yorick <wowyorick@126.com>
2019-04-11 22:05:16 +0800

    source code pro

---
+++ go-tools-install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+goproxy() {
+        export GOPROXY=https://athens.azurefd.net
+        echo "GOPROXY ON"
+}
+
+ungoproxy() {
+unset GOPROXY
+echo "GOPROXY OFF"
+}
+
+
+echo "install gocode"
+go get -u -v github.com/stamblerre/gocode
+echo "install godef"
+go get -u -v github.com/rogpeppe/godef
+
+echo "install dlv"
+go get -u github.com/derekparker/delve/cmd/dlv
+echo "install impl"
+go get -u github.com/josharian/impl
+echo "install gotests"
+go get -u github.com/cweill/gotests/...
+echo "install gomodifytags"
+go get -u github.com/fatih/gomodifytags
+echo "install fillstruct"
+go get -u github.com/davidrjenni/reftools/cmd/fillstruct
+
+goproxy
+echo "install goimports"
+go get -u -v golang.org/x/tools/cmd/goimports
+echo "install guru"
+go get -u -v golang.org/x/tools/cmd/guru
+echo "install gorename"
+go get -u -v golang.org/x/tools/cmd/gorename
+echo "install gotype"
+go get -u golang.org/x/tools/cmd/gotype
+echo "install godoc"
+go get -u golang.org/x/tools/cmd/godoc
+ungoproxy
+echo "plug install finish"
